962b1c34956a9e6bacbb3497d0bda3ed9483936d
wwwDayDream <13383838+wwwDayDream@users.noreply.github.com>
2024-09-03 12:59:02 -0500

    Initial testing commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SkipIntro
+Skips the preloading and intro scenes to go straight to the main menu. Optionally loads a blank scene to improve loading time.
+
+Requires BepInEx Version
+`5.4.2100` [Thunderstore Download](https://thunderstore.io/package/download/BepInEx/BepInExPack/5.4.2100/)
---
+++ WiresSimluationFix/WiresSimulationFixPlugin.fs
@@ -0,0 +1,34 @@
+﻿namespace WiresSimluationFix
+
+open BepInEx
+open HarmonyLib
+open BepInEx.Logging
+open Utils.Operators
+
+module WiresSimluationFix =
+    let mutable internal Plugin: BaseUnityPlugin option = None
+    let mutable internal Logger: ManualLogSource option = None
+    let mutable internal Patcher: Harmony option = None
+    
+open WiresSimluationFix
+
+[<BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)>]
+type WiresSimluationFixPlugin() =
+    inherit BaseUnityPlugin()
+    
+    member this.Awake() =
+        Plugin <- Some this
+        Logger <- Some this.Logger
+        Patcher <- Harmony(this.Info.Metadata.GUID) |> Some
+        
+        Patcher |??> (_.PatchAll()) |>- ()
+        
+[<HarmonyPatch(typeof<SimulationGraph>)>]
+module SimulationGraphPatcher =
+    [<HarmonyPatch(".ctor")>] [<HarmonyPostfix>]
+    let PrepareStartOptionsOverride (__instance: SimulationGraph) =
+        __instance.MaxDeltaTicks |> Logger.Value.LogInfo
+        Traverse.Create(__instance).Field<int>("MaxDeltaTicks").Value <- 1
+        
+        ()
+        
